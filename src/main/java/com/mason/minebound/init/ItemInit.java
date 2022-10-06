95a9e5e496773f8384092d46747b0e030bda45af
Michael Tran <solidwater969@gmail.com>
2022-10-06 17:29:54 -0400

    MB-16 feature: add 11 attack damage to jello item

--- src/main/java/com/mason/minebound/init/ItemInit.java
+++ src/main/java/com/mason/minebound/init/ItemInit.java
@@ -5,6 +5,8 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -14,7 +16,7 @@ import static com.mason.minebound.MineBound.MOD_ID;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-    public static final RegistryObject<Item> JELLO_ITEM = register("jello_item", () -> new Item(new Item.Properties().tab(MINEBOUND_TAB).food(new FoodProperties.Builder().nutrition(8).saturationMod(14.4f).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 200, 0), 0.5F).build())));
+    public static final RegistryObject<Item> JELLO_ITEM = register("jello_item", () -> new SwordItem(Tiers.DIAMOND, 11, 2f, new Item.Properties().tab(MINEBOUND_TAB).food(new FoodProperties.Builder().nutrition(8).saturationMod(14.4f).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 200, 0), 0.5F).build())));
 
     private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item) {
         return ITEMS.register(name, item);
