56c826159cd836d66a39b45d618af7083499f0b7
Michael Tran <solidwater969@gmail.com>
2022-10-06 16:44:19 -0400

    MB-16 feature: add additional properties to jello item

--- src/main/java/com/mason/minebound/init/ItemInit.java
+++ src/main/java/com/mason/minebound/init/ItemInit.java
@@ -1,6 +1,9 @@
 package com.mason.minebound.init;
 
 import com.google.common.base.Supplier;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,7 +14,7 @@ import static com.mason.minebound.MineBound.MOD_ID;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-    public static final RegistryObject<Item> JELLO_ITEM = register("jello_item", () -> new Item(new Item.Properties().tab(MINEBOUND_TAB)));
+    public static final RegistryObject<Item> JELLO_ITEM = register("jello_item", () -> new Item(new Item.Properties().tab(MINEBOUND_TAB).food(new FoodProperties.Builder().nutrition(8).saturationMod(14.4f).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 200, 0), 0.5F).build())));
 
     private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item) {
         return ITEMS.register(name, item);
