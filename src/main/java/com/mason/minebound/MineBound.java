ebe17cb43f151da836e5bdc3bf66867384d668fc
Michael Tran <solidwater969@gmail.com>
2022-10-06 16:54:14 -0400

    MB-16 feature: add translucent to jello crystal block

--- src/main/java/com/mason/minebound/MineBound.java
+++ src/main/java/com/mason/minebound/MineBound.java
@@ -2,6 +2,8 @@ package com.mason.minebound;
 
 import com.mason.minebound.init.BlockInit;
 import com.mason.minebound.init.ItemInit;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
@@ -9,6 +11,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("minebound")
@@ -24,6 +27,11 @@ public class MineBound {
         IEventBus iEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ItemInit.ITEMS.register(iEventBus);
         BlockInit.BLOCKS.register(iEventBus);
+        iEventBus.addListener(this::clientSetup);
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void clientSetup(final FMLClientSetupEvent fmlClientSetupEvent) {
+        ItemBlockRenderTypes.setRenderLayer(BlockInit.JELLO_CRYSTAL_BLOCK.get(), RenderType.translucent());
     }
 }
--- src/main/java/com/mason/minebound/init/BlockInit.java
+++ src/main/java/com/mason/minebound/init/BlockInit.java
@@ -5,6 +5,7 @@ import com.google.common.base.Supplier;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.GlassBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -19,7 +20,7 @@ import static com.mason.minebound.MineBound.MOD_ID;
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
     public static final DeferredRegister<Item> ITEMS = ItemInit.ITEMS;
-    public static final RegistryObject<Block> JELLO_CRYSTAL_BLOCK = register("jello_crystal_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_RED).strength(1.0f).sound(SoundType.METAL)), object -> () -> new BlockItem(object.get(), new Item.Properties().tab(MINEBOUND_TAB)));
+    public static final RegistryObject<Block> JELLO_CRYSTAL_BLOCK = register("jello_crystal_block", () -> new GlassBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_RED).strength(1.0f).sound(SoundType.METAL)), object -> () -> new BlockItem(object.get(), new Item.Properties().tab(MINEBOUND_TAB)));
 
     private static <T extends Block> RegistryObject<T> register(final String name, final Supplier<? extends T> block, Function<RegistryObject<T>, Supplier<? extends Item>> item) {
         RegistryObject<T> registryObject = registerBlock(name, block);
