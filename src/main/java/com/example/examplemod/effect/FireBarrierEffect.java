b7ee8fca74b3087e0cb7eb41e215d5de8c7446ee
Matthew Kirchheimer <mattykirchh@gmail.com>
2022-10-09 23:31:55 -0400

    Cleaned up unnecessary override

--- src/main/java/com/example/examplemod/effect/FireBarrierEffect.java
+++ src/main/java/com/example/examplemod/effect/FireBarrierEffect.java
@@ -2,17 +2,10 @@ package com.example.examplemod.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.entity.LivingEntity;
-import org.jetbrains.annotations.NotNull;
 
 public class FireBarrierEffect extends MobEffect {
     public FireBarrierEffect(MobEffectCategory mobEffectCategory, int color) {
         super(mobEffectCategory, color);
-    }
-
-    @Override
-    public void applyEffectTick(@NotNull LivingEntity livingEntity, int amplifier) {
-        super.applyEffectTick(livingEntity, amplifier);
     }
 
     @Override
