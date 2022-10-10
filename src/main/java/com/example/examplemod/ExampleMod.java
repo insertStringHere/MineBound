351e639692ae4c14e3778e8a63661ca1f8de61cf
Mason <50269959+insertStringHere@users.noreply.github.com>
2022-10-10 15:22:55 -0400

    Revert "MB-17-yonatan-addition"

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,13 +1,10 @@
 package com.example.examplemod;
 
-import com.example.examplemod.item.ModItems;
-import com.example.examplemod.sound.ModSounds;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -25,22 +22,16 @@ import java.util.stream.Collectors;
 public class ExampleMod
 {
     // Directly reference a slf4j logger
-    public static final String MOD_ID = "examplemod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public ExampleMod()
     {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ModItems.register(eventBus);
-        ModSounds.register(eventBus);
-
         // Register the setup method for modloading
-        eventBus.addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
-        eventBus.addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
-        eventBus.addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
