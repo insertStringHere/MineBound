7365899fb2c2a1d28414ce83251af91442e97a44
Matthew Kirchheimer <mattykirchh@gmail.com>
2022-10-09 23:45:24 -0400

    MB 15 fixed effect color

--- src/main/java/com/example/examplemod/effect/ModEffects.java
+++ src/main/java/com/example/examplemod/effect/ModEffects.java
@@ -9,12 +9,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.awt.Color;
+
 public class ModEffects {
   public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS,
       ExampleMod.MOD_ID);
 
   public static final RegistryObject<MobEffect> FIRE_BARRIER = MOB_EFFECTS.register("fire_barrier",
-      () -> new FireBarrierEffect(MobEffectCategory.BENEFICIAL, 3124687));
+      () -> new FireBarrierEffect(MobEffectCategory.BENEFICIAL, new Color(255, 119, 40).getRGB()));
 
   public static void register(IEventBus eventBus) {
     MOB_EFFECTS.register(eventBus);
