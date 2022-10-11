55b8a1ba6e5fbbb8d0d5d3f47de93ef8e8a7f0b0
aat79 <97315750+aat79@users.noreply.github.com>
2022-10-10 23:27:52 -0400

    Added new dusty block

--- src/main/java/com/mineboundteam/minebound/Blocks/BlockInit.java
+++ src/main/java/com/mineboundteam/minebound/Blocks/BlockInit.java
@@ -1,9 +1,13 @@
 package com.mineboundteam.minebound.Blocks;
 
+import com.mineboundteam.minebound.item.ModItems;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,10 +16,16 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BlockInit {
     public static DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "minebound");
     public static RegistryObject<Block> BOOM_BUTTON = BLOCKS.register("boom_button", () -> new BoomButton(Block.Properties.copy(Blocks.STONE_BUTTON)));
+
+    public static final RegistryObject<Block> DUSTY_BLOCK = BLOCKS.register("dusty_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(9f).requiresCorrectToolForDrops()));
 
     @SubscribeEvent
     public static void onRegisterItems(final RegistryEvent.Register<Item> event){
@@ -27,6 +37,8 @@ public class BlockInit {
             registry.register(b);
         }
     }
+
+
 
     public static void register(IEventBus eventBus){
         BlockInit.BLOCKS.register(eventBus);
