66396a59a9591055050bffc37947d5536ebab7f2
Matthew Kirchheimer <mattykirchh@gmail.com>
2022-10-24 16:59:59 -0400

    MB 24 Implemented code to break block

---
+++ src/main/java/com/mineboundteam/minebound/event/Events.java
@@ -0,0 +1,25 @@
+package com.mineboundteam.minebound.event;
+
+
+import com.mineboundteam.minebound.MineBound;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MineBound.MOD_ID)
+public class Events {
+
+    @SubscribeEvent
+    public static void destroyBlock(PlayerInteractEvent.LeftClickBlock event) {
+        if(!event.getEntity().level.isClientSide()) {
+            Player player = event.getPlayer();
+            // Destroy block if clicked on with a stick
+            if(player.getMainHandItem().getItem() == Items.STICK) {
+                // This is really the important code we will likely need down the line
+                event.getWorld().destroyBlock(event.getPos(), true);
+            }
+        }
+    }
+}
