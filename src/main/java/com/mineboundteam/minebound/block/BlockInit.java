b740772c9e1b696fadc20c642b639d162020dc84
Michael Tran <solidwater969@gmail.com>
2022-10-26 14:35:30 -0400

    MB-33 feature: add spell holder

--- src/main/java/com/mineboundteam/minebound/block/BlockInit.java
+++ src/main/java/com/mineboundteam/minebound/block/BlockInit.java
@@ -1,10 +1,6 @@
 package com.mineboundteam.minebound.block;
 
 import com.mineboundteam.minebound.MineBound;
-import com.google.common.base.Function;
-import com.google.common.base.Supplier;
-import com.mineboundteam.minebound.MineBound;
-import com.mineboundteam.minebound.item.ItemInit;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -26,8 +22,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MineBound.MOD_ID);
     public static RegistryObject<Block> BOOM_BUTTON = BLOCKS.register("boom_button", () -> new BoomButton(Block.Properties.copy(Blocks.STONE_BUTTON)));
-    public static final RegistryObject<Block> JELLO_CRYSTAL_BLOCK = BLOCKS.register("jello_crystal_block", () -> new GlassBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_RED).strength(1.0f).sound(SoundType.METAL)));
     public static final RegistryObject<Block> DUSTY_BLOCK = BLOCKS.register("dusty_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> JELLO_CRYSTAL_BLOCK = BLOCKS.register("jello_crystal_block", () -> new GlassBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_RED).strength(1.0f).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> SPELL_HOLDER = BLOCKS.register("spell_holder", () -> new SpellHolder(BlockBehaviour.Properties.of(Material.METAL).strength(99999999999f)));
     public static final RegistryObject<Block> WHEEL_OF_CHEESE = BLOCKS.register("wheel_of_cheese", () -> new Block(Block.Properties.copy(Blocks.CAKE)));
 
     @SubscribeEvent
---
+++ src/main/java/com/mineboundteam/minebound/block/SpellHolder.java
@@ -0,0 +1,29 @@
+package com.mineboundteam.minebound.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class SpellHolder extends Block implements EntityBlock {
+    public SpellHolder(Properties properties) {
+        super(properties);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
+        return EntityBlock.super.getTicker(level, blockState, blockEntityType);
+    }
+}
