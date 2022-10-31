d43d74345004e6149ca07d16b691777a19ef2f5e
Mason Miller <insertstringhere@gmail.com>
2022-10-31 13:25:20 -0400

    MB-20 Add raycasting

--- src/main/java/com/mineboundteam/minebound/MineBound.java
+++ src/main/java/com/mineboundteam/minebound/MineBound.java
@@ -1,6 +1,9 @@
 package com.mineboundteam.minebound;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.mineboundteam.minebound.registry.Registry;
 
 import net.minecraftforge.common.MinecraftForge;
@@ -12,6 +15,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(MineBound.MOD_ID)
 public class MineBound {
     public static final String MOD_ID = "minebound";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public MineBound(){
         IEventBus iEventBus = FMLJavaModLoadingContext.get().getModEventBus();
