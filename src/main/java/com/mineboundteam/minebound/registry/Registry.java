9c36c988adeeba5efe65fc6a2fbd9126b0c8ddb9
Mason Miller <insertstringhere@gmail.com>
2022-11-07 21:45:02 -0500

    MB-30 Add structure spawning

--- src/main/java/com/mineboundteam/minebound/registry/Registry.java
+++ src/main/java/com/mineboundteam/minebound/registry/Registry.java
@@ -5,6 +5,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 public class Registry {
     public static void RegisterMod(IEventBus iEventBus){
         BlockRegistry.BLOCKS.register(iEventBus);
-        
+        ItemRegistry.ITEMS.register(iEventBus);
+        StructureRegistry.STRUCTURES.register(iEventBus);
     }
 }
--- src/main/java/com/mineboundteam/minebound/registry/StructureRegistry.java
+++ src/main/java/com/mineboundteam/minebound/registry/StructureRegistry.java
@@ -1,6 +1,7 @@
 package com.mineboundteam.minebound.registry;
 
 import com.mineboundteam.minebound.MineBound;
+import com.mineboundteam.minebound.structures.NetherTemple;
 
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,5 +10,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class StructureRegistry {
     public static final DeferredRegister<StructureFeature<?>> STRUCTURES = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, MineBound.MOD_ID);
+
+    public static final RegistryObject<StructureFeature<?>> NETHER_TEMPLE = STRUCTURES.register("nether_temple", NetherTemple::new);
     
 }
