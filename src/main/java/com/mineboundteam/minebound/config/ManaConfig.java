ba9c3937df8664d3a8c131aa0312243f0439a813
Mason Miller <insertstringhere@gmail.com>
2023-01-10 18:16:02 -0500

    MBM-366 Add mana recovery data value.

--- src/main/java/com/mineboundteam/minebound/config/ManaConfig.java
+++ src/main/java/com/mineboundteam/minebound/config/ManaConfig.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 public class ManaConfig implements IConfig{
 
     public static IntValue maximumMana;
+    public static IntValue manaRecovery;
 
     /**
      * Sets up the section of the config file to manage mana values
@@ -19,6 +20,7 @@ public class ManaConfig implements IConfig{
         builder.push("Mana Managment");
         
         maximumMana = builder.comment("The maximum amount of mana a player has without any augmentation. [Default = 200]").defineInRange("Max_Mana", 200, 0, 10000);  
+        manaRecovery = builder.comment("The recovery rate of mana per second by the player without augmentation. [Default = 1]").defineInRange("Mana_Recovery", 1, 0, 10000);
 
         builder.pop();
     }
