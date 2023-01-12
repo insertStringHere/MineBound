bb0a5996320948d464aa0c373c75506ef1764744
BuildTools <unconfigured@null.spigotmc.org>
2023-01-11 22:28:46 -0500

    MBM-257 My'ri Manasac Added My'ri Manasac and MineBound creative tab

--- src/main/java/com/mineboundteam/minebound/registry/ItemRegistry.java
+++ src/main/java/com/mineboundteam/minebound/registry/ItemRegistry.java
@@ -5,7 +5,10 @@ import com.mineboundteam.minebound.MineBound;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MineBound.MOD_ID);
+
+    public static final RegistryObject<Item> MYRI_MANASAC = ITEMS.register("myri_manasac", () -> new Item(new Item.Properties().tab(MineBound.MINEBOUND_TAB)));
 }
