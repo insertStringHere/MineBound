b03af29a52966c6916563c61a4a2860b2067a993
BuildTools <unconfigured@null.spigotmc.org>
2023-01-11 23:00:00 -0500

    MBM-251 Myrial Steel Ingot Added Myrial Steel Ingot item

--- src/main/java/com/mineboundteam/minebound/registry/ItemRegistry.java
+++ src/main/java/com/mineboundteam/minebound/registry/ItemRegistry.java
@@ -12,6 +12,7 @@ public class ItemRegistry {
 
     public static final RegistryObject<Item> GILDED_DIAMOND_INGOT = ITEMS.register("gilded_diamond_ingot", () -> new Item(new Item.Properties().tab(MineBound.MINEBOUND_TAB)));
     public static final RegistryObject<Item> MYRIAL_DIAMOND_INGOT = ITEMS.register("myrial_diamond_ingot", () -> new Item(new Item.Properties().tab(MineBound.MINEBOUND_TAB)));
+    public static final RegistryObject<Item> MYRIAL_STEEL_INGOT = ITEMS.register("myrial_steel_ingot", () -> new Item(new Item.Properties().tab(MineBound.MINEBOUND_TAB)));
     public static final RegistryObject<Item> MYRI_CHUNK = ITEMS.register("myri_chunk", () -> new Item(new Item.Properties().tab(MineBound.MINEBOUND_TAB)));
     public static final RegistryObject<Item> MYRI_MANASAC = ITEMS.register("myri_manasac", () -> new Item(new Item.Properties().tab(MineBound.MINEBOUND_TAB)));
 }
