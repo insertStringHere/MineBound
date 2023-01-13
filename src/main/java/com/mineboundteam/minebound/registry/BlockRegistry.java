f2e0b254dfb37676d15559eb29799b40a09a3d6e
Mason Miller <insertstringhere@gmail.com>
2023-01-13 16:00:08 -0500

    MBM-45 Add myrial console block

--- src/main/java/com/mineboundteam/minebound/registry/BlockRegistry.java
+++ src/main/java/com/mineboundteam/minebound/registry/BlockRegistry.java
@@ -2,13 +2,34 @@ package com.mineboundteam.minebound.registry;
 
 import com.mineboundteam.minebound.MineBound;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.RegistryObject;
 
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MineBound.MOD_ID);
-
     
-}
+    public static final RegistryObject<Block> MYRIAL_CONSOLE = BLOCKS.register("myrial_console", () -> new Block(Properties.copy(Blocks.NETHERITE_BLOCK).lightLevel((i) -> 10)));
+    
+    @SubscribeEvent
+    public static void RegisterBlockItems(RegistryEvent.Register<Item> event){
+        IForgeRegistry<Item> registry = event.getRegistry();
+        for(RegistryObject<Block> b : BLOCKS.getEntries()){
+            BlockItem blockItem = new BlockItem(b.get(), new Item.Properties().tab(MineBound.MINEBOUND_TAB));
+            blockItem.setRegistryName(b.getId());
+            if(!registry.containsKey(blockItem.getRegistryName()))
+                registry.register(blockItem);
+        }
+    }
+
+}   
