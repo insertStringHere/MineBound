46b7ae8869ae65ecbb1ca691188cce8f7a2decff
insertStringHere <insertstringhere@gmail.com>
2023-01-19 01:06:33 -0500

    MBM-46 Fix block orientation

--- src/main/java/com/mineboundteam/minebound/registry/BlockRegistry.java
+++ src/main/java/com/mineboundteam/minebound/registry/BlockRegistry.java
@@ -1,7 +1,7 @@
 package com.mineboundteam.minebound.registry;
 
 import com.mineboundteam.minebound.MineBound;
-import com.mineboundteam.minebound.block.MyrialConsoleBlock;
+import com.mineboundteam.minebound.block.MultiDirectionalBlock;
 
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -22,10 +22,10 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MineBound.MOD_ID);
 
-    public static final RegistryObject<Block> MYRIAL_CONSOLE = BLOCKS.register("myrial_console", () -> new MyrialConsoleBlock(Properties.copy(Blocks.NETHERITE_BLOCK).lightLevel((i) -> 8)));
+    public static final RegistryObject<Block> MYRIAL_CONSOLE = BLOCKS.register("myrial_console", () -> new MultiDirectionalBlock(Properties.copy(Blocks.NETHERITE_BLOCK).lightLevel((i) -> 8)));
     public static final RegistryObject<Block> MYRIAL_GLASS = BLOCKS.register("myrial_glass", () -> new Block(Properties.copy(Blocks.GLASS).noOcclusion().requiresCorrectToolForDrops().strength(30f, 500f)));
     public static final RegistryObject<Block> MYRIAL_STEEL_BLOCK = BLOCKS.register("myrial_steel_block", () -> new Block(Properties.copy(Blocks.NETHERITE_BLOCK)));
-    public static final RegistryObject<Block> MYRIAL_LOCKER = BLOCKS.register("myrial_locker", () -> new Block(Properties.copy(Blocks.NETHERITE_BLOCK)));
+    public static final RegistryObject<Block> MYRIAL_LOCKER = BLOCKS.register("myrial_locker", () -> new MultiDirectionalBlock(Properties.copy(Blocks.NETHERITE_BLOCK)));
 
     @SubscribeEvent
     public static void RegisterBlockItems(RegistryEvent.Register<Item> event){
