28e67ef94f2fb32306006f6c0e294208c95e458f
Matthew Kirchheimer <mattykirchh@gmail.com>
2023-02-03 20:29:42 -0500

    MBM-286 Added core code, still missing if spell equipped check

--- src/main/java/com/mineboundteam/minebound/magic/UtilitySpells/TelekineticUtilitySpell.java
+++ src/main/java/com/mineboundteam/minebound/magic/UtilitySpells/TelekineticUtilitySpell.java
@@ -1,16 +1,21 @@
 package com.mineboundteam.minebound.magic.UtilitySpells;
 
+import com.mineboundteam.minebound.MineBound;
 import com.mineboundteam.minebound.config.IConfig;
 import com.mineboundteam.minebound.item.armor.MyrialArmorItem;
 import com.mineboundteam.minebound.magic.PassiveSpellItem;
 import com.mineboundteam.minebound.magic.SpellLevel;
 import com.mineboundteam.minebound.mana.PlayerManaProvider;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.ForgeConfigSpec.Builder;
 import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
+
+@Mod.EventBusSubscriber(modid = MineBound.MOD_ID)
 public class TelekineticUtilitySpell extends PassiveSpellItem {
 
     private final int manaCost;
@@ -24,13 +29,19 @@ public class TelekineticUtilitySpell extends PassiveSpellItem {
     @Override
     public void updateEquipment(MyrialArmorItem item, Player player) {
         super.updateEquipment(item, player);
+        //TODO: check if spell is equipped in a utility slot, then do this
+        player.getAbilities().mayfly = !player.getAbilities().mayfly;
+        player.onUpdateAbilities();
+    }
 
-        // This shit definitely ain't working
-        if (player.getAbilities().flying) {
-            player.getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(mana -> player.displayClientMessage(
-                    new TextComponent(String.format("Player currently has %d mana", mana.getMana())), false));
-
-            super.reduceMana(manaCost, player);
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.side.isServer() && event.player.level.getDayTime() % 20 == 0 && event.phase == TickEvent.Phase.START && event.player.getAbilities().mayfly) {
+            event.player.getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(mana -> {
+                if (event.player.getAbilities().flying) {
+                    super.reduceMana(manaCost, event.player);
+                }
+            });
         }
     }
 
