84ddafaf03e6b65a433a1e6b82b8d4038843448e
Michael Tran <solidwater969@gmail.com>
2023-02-11 23:10:27 -0500

    MBM-338 MBM-342 MBM-346 MBM-350 MBM-357 refactor | undo unintented changes part 2

--- src/main/java/com/mineboundteam/minebound/event/ModEvents.java
+++ src/main/java/com/mineboundteam/minebound/event/ModEvents.java
@@ -10,6 +10,7 @@ import com.mineboundteam.minebound.mana.PlayerManaProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
@@ -21,7 +22,7 @@ import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.system.CallbackI;
 
-import java.util.*;import net.minecraft.world.entity.player.Player;
+import java.util.*;
 
 @Mod.EventBusSubscriber(modid = MineBound.MOD_ID)
 public class ModEvents {
