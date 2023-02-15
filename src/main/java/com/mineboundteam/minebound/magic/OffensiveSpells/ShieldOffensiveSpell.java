44f8ef54ec8d9a96cd3811dc2f5ad01766a95c69
Matthew Kirchheimer <mattykirchh@gmail.com>
2023-02-14 19:32:29 -0500

    MBM-289 Added spell values to config and changed how spell is activated

--- src/main/java/com/mineboundteam/minebound/magic/OffensiveSpells/ShieldOffensiveSpell.java
+++ src/main/java/com/mineboundteam/minebound/magic/OffensiveSpells/ShieldOffensiveSpell.java
@@ -4,6 +4,7 @@ import com.mineboundteam.minebound.MineBound;
 import com.mineboundteam.minebound.config.IConfig;
 import com.mineboundteam.minebound.item.armor.ArmorTier;
 import com.mineboundteam.minebound.magic.ActiveSpellItem;
+import com.mineboundteam.minebound.registry.ClientRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
@@ -11,66 +12,64 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
 import java.util.List;
 
 public class ShieldOffensiveSpell extends ActiveSpellItem {
 
     private final int manaCost;
+    private final double damageReduction;
+    private final double damageReflected;
     private boolean active = false;
-    private static final HashMap<ArmorTier, double[]> spellLevelDmgMult = new HashMap<>() {{
-        // Reduce damage by 50% and reflect 40%
-        put(ArmorTier.EFFIGY, new double[]{0.5, 0.4});
-        // Reduce damage by 70% and reflect 60%
-        put(ArmorTier.SUIT, new double[]{0.7, 0.6});
-        // Reduce damage by 100% and reflect 80%
-        put(ArmorTier.SYNERGY, new double[]{1.0, 0.8});
-    }};
 
     public ShieldOffensiveSpell(Properties properties, ShieldOffensiveSpellConfig config) {
         super(properties, config.LEVEL);
 
         this.manaCost = config.MANA_COST.get();
+        this.damageReduction = config.DMG_REDUCTION.get();
+        this.damageReflected = config.DMG_REFLECTED.get();
+
+        MineBound.registerObject(this);
     }
 
-    // TODO: this code will execute in a different function once we work out how to execute spells from the keybindings
+    // TODO: tie in with keybindings based on where spell is equipped
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if(event.side.isServer() && event.phase == TickEvent.Phase.START) {
+            active = ClientRegistry.PRIMARY_MAGIC.isDown();
+        }
+    }
+
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        if (!level.isClientSide()) {
-            active = !active;
-            player.displayClientMessage(new TextComponent("Active: " + active), false);
-            // `triggerSpell` will only be called when spell is active
-            // my theory is this is more performant than checking `active` in `triggerSpell`
-            if (active) {
-                MineBound.registerObject(this);
-            } else {
-                MineBound.unregisterObject(this);
-            }
-        }
-        return InteractionResultHolder.success(player.getItemInHand(usedHand));
+        return null;
     }
 
     @SubscribeEvent
     public void triggerSpell(LivingAttackEvent event) {
-        if (!event.getEntity().level.isClientSide() && event.getEntityLiving() instanceof Player player) {
+        if (active && !event.getEntity().level.isClientSide() && event.getEntityLiving() instanceof Player player) {
             Entity sourceEntity = event.getSource().getEntity();
             if (sourceEntity != null) {
                 float dmgAmount = event.getAmount();
-                if ((1 - spellLevelDmgMult.get(level)[0]) == 0) {
+                if ((1 - damageReduction) == 0) {
                     event.setCanceled(true);
                 }
-                sourceEntity.hurt(DamageSource.thorns(player), (float) (dmgAmount * spellLevelDmgMult.get(level)[1]));
+                sourceEntity.hurt(DamageSource.thorns(player), (float) (dmgAmount * damageReflected));
                 super.reduceMana(manaCost, player);
             }
         }
@@ -78,12 +77,12 @@ public class ShieldOffensiveSpell extends ActiveSpellItem {
 
     @SubscribeEvent
     public void triggerSpell(LivingHurtEvent event) {
-        if (!event.getEntity().level.isClientSide() && event.getEntityLiving() instanceof Player) {
+        if (active && !event.getEntity().level.isClientSide() && event.getEntityLiving() instanceof Player) {
             Entity sourceEntity = event.getSource().getEntity();
             if (sourceEntity != null) {
                 float dmgAmount = event.getAmount();
-                if ((1 - spellLevelDmgMult.get(level)[0]) != 0) {
-                    event.setAmount((float) (dmgAmount * (1 - spellLevelDmgMult.get(level)[0])));
+                if ((1 - damageReduction) != 0) {
+                    event.setAmount((float) (dmgAmount * (1 - damageReduction)));
                 }
                 // LivingAttackEvent will fall through to LivingHurtEvent if not canceled, thus no need to thorns and reduce mana here
             }
@@ -94,9 +93,9 @@ public class ShieldOffensiveSpell extends ActiveSpellItem {
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
         pTooltipComponents.add(new TextComponent("While active:").withStyle(ChatFormatting.GRAY));
         pTooltipComponents.add(new TextComponent("  - Reduces damage taken from mobs by ").withStyle(ChatFormatting.GRAY)
-                                       .append(new TextComponent((int) (spellLevelDmgMult.get(level)[0] * 100) + "%").withStyle(ChatFormatting.GOLD).withStyle(ChatFormatting.UNDERLINE)));
+                                       .append(new TextComponent((int) (damageReduction * 100) + "%").withStyle(ChatFormatting.GOLD).withStyle(ChatFormatting.UNDERLINE)));
         pTooltipComponents.add(new TextComponent("  - Reflects ").withStyle(ChatFormatting.GRAY)
-                                       .append(new TextComponent((int) (spellLevelDmgMult.get(level)[1] * 100) + "%").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.UNDERLINE))
+                                       .append(new TextComponent((int) (damageReflected * 100) + "%").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.UNDERLINE))
                                        .append(" of the initial damage").withStyle(ChatFormatting.GRAY));
         pTooltipComponents.add(new TextComponent("Costs ").withStyle(ChatFormatting.GRAY)
                                        // TODO: Color subject to change once mana UI is implemented
@@ -107,22 +106,30 @@ public class ShieldOffensiveSpell extends ActiveSpellItem {
 
     public static class ShieldOffensiveSpellConfig implements IConfig {
 
-        public ForgeConfigSpec.IntValue MANA_COST;
+        public IntValue MANA_COST;
+        public DoubleValue DMG_REDUCTION;
+        public DoubleValue DMG_REFLECTED;
         public final ArmorTier LEVEL;
 
         private final int manaCost;
+        private final double damageReduction;
+        private final double damageReflected;
 
-        public ShieldOffensiveSpellConfig(int manaCost, ArmorTier level) {
+        public ShieldOffensiveSpellConfig(int manaCost, double damageReduction, double damageReflected, ArmorTier level) {
             this.manaCost = manaCost;
+            this.damageReduction = damageReduction;
+            this.damageReflected = damageReflected;
             this.LEVEL = level;
         }
 
         @Override
         public void build(ForgeConfigSpec.Builder builder) {
-            builder.push("Shield Offensive");
-            builder.push("Tier " + LEVEL.getValue());
+            builder.push("Offensive");
+            builder.push(LEVEL.toString());
             MANA_COST = builder.comment("Mana cost").defineInRange("mana_cost", manaCost, 0, 10000);
-            builder.pop();
+            DMG_REDUCTION = builder.comment("Damage Reduction").defineInRange("dmg_reduction", damageReduction, 0.0, 1.0);
+            DMG_REFLECTED = builder.comment("Damage Reflected").defineInRange("dmg_reflected", damageReflected, 0.0, 10.0);
+            builder.pop(2);
         }
 
         @Override
--- src/main/java/com/mineboundteam/minebound/registry/config/MagicConfigRegistry.java
+++ src/main/java/com/mineboundteam/minebound/registry/config/MagicConfigRegistry.java
@@ -2,8 +2,8 @@ package com.mineboundteam.minebound.registry.config;
 
 import com.mineboundteam.minebound.config.IConfig;
 import com.mineboundteam.minebound.item.armor.ArmorTier;
-import com.mineboundteam.minebound.magic.OffensiveSpells.TestSpell;
 import com.mineboundteam.minebound.magic.OffensiveSpells.ShieldOffensiveSpell;
+import com.mineboundteam.minebound.magic.OffensiveSpells.TestSpell;
 import com.mineboundteam.minebound.magic.UtilitySpells.TelekineticUtilitySpell;
 import net.minecraftforge.common.ForgeConfigSpec.Builder;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
@@ -13,9 +13,9 @@ public class MagicConfigRegistry implements IConfig {
     public static final TestSpell.TestSpellConfig TEST_SPELL = new TestSpell.TestSpellConfig(10, ArmorTier.EFFIGY);
 
     /* Shield */
-    public static final ShieldOffensiveSpell.ShieldOffensiveSpellConfig SHIELD_OFFENSIVE_1 = new ShieldOffensiveSpell.ShieldOffensiveSpellConfig(30, ArmorTier.EFFIGY);
-    public static final ShieldOffensiveSpell.ShieldOffensiveSpellConfig SHIELD_OFFENSIVE_2 = new ShieldOffensiveSpell.ShieldOffensiveSpellConfig(40, ArmorTier.SUIT);
-    public static final ShieldOffensiveSpell.ShieldOffensiveSpellConfig SHIELD_OFFENSIVE_3 = new ShieldOffensiveSpell.ShieldOffensiveSpellConfig(50, ArmorTier.SYNERGY);
+    public static final ShieldOffensiveSpell.ShieldOffensiveSpellConfig SHIELD_OFFENSIVE_1 = new ShieldOffensiveSpell.ShieldOffensiveSpellConfig(30, 0.5, 0.4, ArmorTier.EFFIGY);
+    public static final ShieldOffensiveSpell.ShieldOffensiveSpellConfig SHIELD_OFFENSIVE_2 = new ShieldOffensiveSpell.ShieldOffensiveSpellConfig(40, 0.7, 0.6, ArmorTier.SUIT);
+    public static final ShieldOffensiveSpell.ShieldOffensiveSpellConfig SHIELD_OFFENSIVE_3 = new ShieldOffensiveSpell.ShieldOffensiveSpellConfig(50, 1.0, 0.8, ArmorTier.SYNERGY);
 
     /* Telekinetic */
     public static final TelekineticUtilitySpell.TelekineticUtilitySpellConfig TELEKINETIC_UTILITY_2 = new TelekineticUtilitySpell.TelekineticUtilitySpellConfig(10, 0.20, ArmorTier.SUIT);
@@ -28,6 +28,17 @@ public class MagicConfigRegistry implements IConfig {
         TEST_SPELL.build(builder);
         builder.pop();
 
+        /*
+         * Spell Element
+         * [offensive spells]
+         * [defensive spells]
+         * [utility spells]
+         * Follow this ordering to ensure config is generated the same for all spells
+         */
+
+        builder.push("Fire");
+        builder.pop();
+
         builder.push("Telekinetic");
         TELEKINETIC_UTILITY_2.build(builder);
         TELEKINETIC_UTILITY_3.build(builder);
@@ -38,6 +49,21 @@ public class MagicConfigRegistry implements IConfig {
         SHIELD_OFFENSIVE_1.build(builder);
         SHIELD_OFFENSIVE_2.build(builder);
         SHIELD_OFFENSIVE_3.build(builder);
+        builder.pop();
+
+        builder.push("Earth");
+        builder.pop();
+
+        builder.push("Ender");
+        builder.pop();
+
+        builder.push("Electric");
+        builder.pop();
+
+        builder.push("Light");
+        builder.pop();
+
+        builder.push("Necrotic");
         builder.pop();
     }
 
