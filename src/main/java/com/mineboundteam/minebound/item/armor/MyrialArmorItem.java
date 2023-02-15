a0a3028bfff84b3177f7ad76d791f300c7ff0564
Matthew Kirchheimer <mattykirchh@gmail.com>
2023-02-14 20:39:09 -0500

    MBM-391 Added tooltips

--- src/main/java/com/mineboundteam/minebound/item/armor/MyrialArmorItem.java
+++ src/main/java/com/mineboundteam/minebound/item/armor/MyrialArmorItem.java
@@ -1,12 +1,18 @@
 package com.mineboundteam.minebound.item.armor;
 
-import java.util.function.Consumer;
-
+import com.mineboundteam.minebound.MineBound;
 import com.mineboundteam.minebound.config.ArmorConfig;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -18,29 +24,56 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.item.GeoArmorItem;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class MyrialArmorItem extends GeoArmorItem implements IAnimatable {
     private final ArmorConfig config;
     private final ArmorTier tier;
+    public static final ChatFormatting[] tierColors = new ChatFormatting[]{
+            ChatFormatting.YELLOW,
+            ChatFormatting.GOLD,
+            ChatFormatting.DARK_RED,
+            ChatFormatting.DARK_AQUA
+    };
 
     private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
 
     public MyrialArmorItem(ArmorMaterial pMaterial, EquipmentSlot pSlot, Properties pProperties, ArmorTier pTier, ArmorConfig pConfig) {
         super(pMaterial, pSlot, pProperties.durability(pConfig.ENERGY.get()));
         this.config = pConfig;
-        this.tier = pTier;        
+        this.tier = pTier;
     }
 
     @Override
-    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken){
+    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
         // Remove normal damage, we're not using durability for durability
         return 0;
     }
 
-    public ArmorConfig getConfig(){
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(new TextComponent("Tier ").withStyle(tierColors[tier.getValue()])
+                                       .append(new TranslatableComponent("tooltip." + MineBound.MOD_ID + ".level." + tier.getValue()).withStyle(tierColors[tier.getValue()])));
+        pTooltipComponents.add(new TextComponent("Manapool: ").withStyle(ChatFormatting.GRAY)
+                                       .append(new TextComponent("+" + config.MANAPOOL.get()).withStyle(ChatFormatting.BLUE)));
+        if (config.RECOVERY.get() > 0) {
+            pTooltipComponents.add(new TextComponent("Mana Recovery: ").withStyle(ChatFormatting.GRAY)
+                                           .append(new TextComponent("+" + config.RECOVERY.get() + "/sec").withStyle(ChatFormatting.BLUE)));
+        }
+        pTooltipComponents.add(new TextComponent("Active Magic slots: ").withStyle(ChatFormatting.GRAY)
+                                       .append(new TextComponent(config.STORAGE_SLOTS.get() + "").withStyle(ChatFormatting.RED)));
+        pTooltipComponents.add(new TextComponent("Utility Magic slots: ").withStyle(ChatFormatting.GRAY)
+                                       .append(new TextComponent(config.UTILITY_SLOTS.get() + "").withStyle(ChatFormatting.DARK_PURPLE)));
+        pTooltipComponents.add(new TextComponent("Total Energy: ").withStyle(ChatFormatting.GRAY)
+                                       .append(new TextComponent(config.ENERGY.get() + "").withStyle(ChatFormatting.GREEN)));
+    }
+
+    public ArmorConfig getConfig() {
         return config;
     }
-    
-    public ArmorTier getTier(){
+
+    public ArmorTier getTier() {
         return tier;
     }
 
@@ -49,7 +82,7 @@ public class MyrialArmorItem extends GeoArmorItem implements IAnimatable {
         data.addAnimationController(new AnimationController<MyrialArmorItem>(this, getDescriptionId(), 20, this::predicate));
     }
 
-    private <T extends IAnimatable> PlayState predicate(AnimationEvent<T> event){
+    private <T extends IAnimatable> PlayState predicate(AnimationEvent<T> event) {
         event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", EDefaultLoopTypes.LOOP));
         return PlayState.CONTINUE;
     }
