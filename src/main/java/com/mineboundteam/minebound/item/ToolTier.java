65b67a03ea8d4100985db2bf7c9c0083ef057154
Yonatan <yw692@drexel.edu>
2023-02-15 14:27:47 -0500

    MB-339: Updated Tool Tier for Myrial Diamond

--- src/main/java/com/mineboundteam/minebound/item/ToolTier.java
+++ src/main/java/com/mineboundteam/minebound/item/ToolTier.java
@@ -10,5 +10,5 @@ public class ToolTier {
     public static final ForgeTier ENERGIZED_IRON = new ForgeTier(2, 250, 7, 2, 8, BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.of(ItemRegistry.ENERGIZED_IRON_INGOT.get()));
     public static final ForgeTier ILLUMINANT_STEEL = new ForgeTier(2, 1561, 7.5F, 2.3F, 8, BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.of(ItemRegistry.ILLUMINANT_STEEL_INGOT.get()));
     public static final ForgeTier GILDED_DIAMOND = new ForgeTier(3, 1700, 10.5F, 3, 16, BlockTags.NEEDS_DIAMOND_TOOL, () -> Ingredient.of(ItemRegistry.GILDED_DIAMOND_INGOT.get()));
-    public static ForgeTier MYRIAL_DIAMOND = new ForgeTier(5, 2750, 15F, 5, 20, Tags.Blocks.NEEDS_NETHERITE_TOOL, () -> Ingredient.of(ItemRegistry.MYRIAL_DIAMOND_INGOT.get()));
+    public static ForgeTier MYRIAL_DIAMOND = new ForgeTier(5, 2750, 10F, 3, 11, Tags.Blocks.NEEDS_NETHERITE_TOOL, () -> Ingredient.of(ItemRegistry.MYRIAL_DIAMOND_INGOT.get()));
 }
