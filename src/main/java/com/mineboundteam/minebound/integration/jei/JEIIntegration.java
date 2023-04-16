c3ed2d9481189688ebcadac137ff422e4a856bfb
insertStringHere <insertstringhere@gmail.com>
2023-04-16 16:04:21 -0400

    MBM-406 Finish JEI integration

--- src/main/java/com/mineboundteam/minebound/integration/jei/JEIIntegration.java
+++ src/main/java/com/mineboundteam/minebound/integration/jei/JEIIntegration.java
@@ -1,41 +1,44 @@
 package com.mineboundteam.minebound.integration.jei;
 
 import com.mineboundteam.minebound.MineBound;
+import com.mineboundteam.minebound.block.registry.BlockRegistry;
 import com.mineboundteam.minebound.crafting.AlloyFurnaceRecipe;
 import com.mineboundteam.minebound.crafting.ArmorForgeRecipe;
+import com.mineboundteam.minebound.inventory.AlloyFurnaceMenu;
+import com.mineboundteam.minebound.inventory.ArmorForgeMenu;
 import com.mineboundteam.minebound.inventory.registry.RecipeRegistry;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.helpers.IJeiHelpers;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.transfer.IRecipeTransferError;
+import mezz.jei.api.recipe.transfer.IRecipeTransferHandler;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
-import mezz.jei.api.runtime.IJeiRuntime;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeManager;
 
 @JeiPlugin
 public class JEIIntegration implements IModPlugin{
 
-    public static IJeiHelpers helpers;
-
-    public static RecipeType<AlloyFurnaceRecipe> ALLOY_FURNACE_RECIPE_TYPE = RecipeType.create(MineBound.MOD_ID, "alloy_furnace", AlloyFurnaceRecipe.class); 
-    public static RecipeType<ArmorForgeRecipe> ARMOR_FORGE_RECIPE_TYPE = RecipeType.create(MineBound.MOD_ID, "armor_forge", ArmorForgeRecipe.class); 
+    public static final RecipeType<ArmorForgeRecipe> ARMOR_FORGE = new RecipeType<>(RecipeRegistry.ARMOR_FORGE_RECIPE.getId(), ArmorForgeRecipe.class);
+    public static final RecipeType<AlloyFurnaceRecipe> ALLOY_FURNACE = new RecipeType<>(RecipeRegistry.ALLOY_FURNACE_RECIPE.getId(), AlloyFurnaceRecipe.class);
 
     @Override
     public ResourceLocation getPluginUid() {
         return new ResourceLocation(MineBound.MOD_ID, "jei");
     }
 
-    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
-        helpers = jeiRuntime.getJeiHelpers();
-	}
-
     @Override
 	public void registerCategories(IRecipeCategoryRegistration registration) {
-        registration.addRecipeCategories(new ArmorForgeRecipeCategory());
-        registration.addRecipeCategories(new AlloyFurnaceRecipeCategory());
+        registration.addRecipeCategories(new ArmorForgeRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new AlloyFurnaceRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
 	}
 
     @Override
@@ -43,10 +46,24 @@ public class JEIIntegration implements IModPlugin{
 	public void registerRecipes(IRecipeRegistration registration) {
         ClientLevel world = Minecraft.getInstance().level;
         if (world == null) return;
+
+        RecipeManager rm = world.getRecipeManager();
         
-        registration.addRecipes(ARMOR_FORGE_RECIPE_TYPE, world.getRecipeManager().getAllRecipesFor(RecipeRegistry.ARMOR_FORGE_RECIPE.get()));
-        registration.addRecipes(ALLOY_FURNACE_RECIPE_TYPE, world.getRecipeManager().getAllRecipesFor(RecipeRegistry.ALLOY_FURNACE_RECIPE.get()));
+        registration.addRecipes(ARMOR_FORGE, rm.getAllRecipesFor(RecipeRegistry.ARMOR_FORGE_RECIPE.get()));
+        registration.addRecipes(ALLOY_FURNACE, rm.getAllRecipesFor(RecipeRegistry.ALLOY_FURNACE_RECIPE.get()));          
     }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(BlockRegistry.ARMOR_FORGE.get()), ARMOR_FORGE);
+        registration.addRecipeCatalyst(new ItemStack(BlockRegistry.ALLOY_FURNACE.get()), ALLOY_FURNACE);
+	}
+
+    @Override
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+        registration.addRecipeTransferHandler(ArmorForgeMenu.class, ARMOR_FORGE, 40, 6, 0, 40);
+        registration.addRecipeTransferHandler(AlloyFurnaceMenu.class, ALLOY_FURNACE, 0, 3, 5, 36);
+	}
 
     
 }
