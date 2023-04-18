cc2c4006fe78e66619923a54b50ae3be76fe1894
Michael Tran <solidwater969@gmail.com>
2023-04-18 09:30:52 -0400

    MBM-361 MBM-353 refactor | replace class var with nbt

--- src/main/java/com/mineboundteam/minebound/entity/MyrialSwordEntity.java
+++ src/main/java/com/mineboundteam/minebound/entity/MyrialSwordEntity.java
@@ -1,31 +1,29 @@
 package com.mineboundteam.minebound.entity;
 
-import com.mineboundteam.minebound.item.MyrialSword;
-import com.mineboundteam.minebound.registry.EntityRegistry;
-import com.mineboundteam.minebound.registry.ItemRegistry;
+import com.mineboundteam.minebound.entity.registry.EntityRegistry;
+import com.mineboundteam.minebound.item.registry.ItemRegistry;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
 public class MyrialSwordEntity extends ThrowableItemProjectile {
-    MyrialSword myrialSword;
+    ItemStack itemStack;
 
     public MyrialSwordEntity(EntityType<MyrialSwordEntity> entityType, Level level) {
         super(entityType, level);
     }
 
-    public MyrialSwordEntity(Level level, Player shooter, MyrialSword myrialSword) {
+    public MyrialSwordEntity(Level level, Player shooter, ItemStack itemStack) {
         super(EntityRegistry.MYRIAL_SWORD_ENTITY.get(), shooter, level);
-        this.myrialSword = myrialSword;
+        this.itemStack = itemStack;
     }
 
     @Override
@@ -46,11 +44,11 @@ public class MyrialSwordEntity extends ThrowableItemProjectile {
     @Override
     public void onHitEntity(EntityHitResult entityHitResult) {
         Entity entity = entityHitResult.getEntity();
-        if (getOwner() == null || myrialSword == null) return;
+        if (getOwner() == null || itemStack == null) return;
 
         if (entity.is(getOwner())) {
             discard();
-            myrialSword.state = 0;
+            itemStack.getOrCreateTag().putInt("minebound.myrial_sword", 0);
         } else {
             entity.hurt(DamageSource.MAGIC, 7);
         }
@@ -60,11 +58,11 @@ public class MyrialSwordEntity extends ThrowableItemProjectile {
     public void tick() {
         setYRot(getYRot() + 100);
         super.tick();
-        if (myrialSword == null || getOwner() == null) return;
+        if (itemStack == null || getOwner() == null) return;
 
         if (getOwner().position().distanceTo(position()) > 15)
             setDeltaMovement(Vec3.ZERO);
-        if (myrialSword.state == 2)
+        if (itemStack.getOrCreateTag().getInt("minebound.myrial_sword") == 2)
             setDeltaMovement(getDeltaMovement().scale(.95).add(getOwner().position().subtract(position()).normalize().scale(.5)));
     }
 }
--- src/main/java/com/mineboundteam/minebound/item/MyrialSword.java
+++ src/main/java/com/mineboundteam/minebound/item/MyrialSword.java
@@ -4,31 +4,30 @@ import com.mineboundteam.minebound.entity.MyrialSwordEntity;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 public class MyrialSword extends SwordItem {
     MyrialSwordEntity myrialSwordEntity;
-    public int state = 0;
+    ItemStack itemStack;
 
     public MyrialSword(Tier tier, int attackDamageModifier, float attackSpeedModifier, Properties properties) {
         super(tier, attackDamageModifier, attackSpeedModifier, properties);
+        itemStack = new ItemStack(this);
     }
 
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand interactionHand) {
         if (level.isClientSide) return super.use(level, player, interactionHand);
 
-        if (state == 0) {
-            myrialSwordEntity = new MyrialSwordEntity(level, player, this);
+        if (itemStack.getOrCreateTag().getInt("minebound.myrial_sword") == 0) {
+            myrialSwordEntity = new MyrialSwordEntity(level, player, itemStack);
             myrialSwordEntity.shootFromRotation(player, player.getXRot(), player.getYRot(), 0, 1.5f, 1);
             level.addFreshEntity(myrialSwordEntity);
-            state = 1;
-        } else if (state == 1) {
-            state = 2;
+            itemStack.getOrCreateTag().putInt("minebound.myrial_sword", 1);
+        } else if (itemStack.getOrCreateTag().getInt("minebound.myrial_sword") == 1) {
+            itemStack.getOrCreateTag().putInt("minebound.myrial_sword", 2);
         }
 
         return super.use(level, player, interactionHand);
