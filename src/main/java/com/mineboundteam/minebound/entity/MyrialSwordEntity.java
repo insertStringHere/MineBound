363980892cccc009c9ec315212a15eb3113918c8
Michael Tran <solidwater969@gmail.com>
2023-04-20 20:42:49 -0400

    MBM-353 feature | add mobbing

--- src/main/java/com/mineboundteam/minebound/entity/MyrialSwordEntity.java
+++ src/main/java/com/mineboundteam/minebound/entity/MyrialSwordEntity.java
@@ -2,18 +2,24 @@ package com.mineboundteam.minebound.entity;
 
 import com.mineboundteam.minebound.entity.registry.EntityRegistry;
 import com.mineboundteam.minebound.item.registry.ItemRegistry;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
 public class MyrialSwordEntity extends ThrowableItemProjectile {
+    int maxEntitiesHit = 16;
+    IntOpenHashSet entitiesHit = new IntOpenHashSet(maxEntitiesHit);
     ItemStack myrialSwordVacuum;
 
     public MyrialSwordEntity(EntityType<MyrialSwordEntity> entityType, Level level) {
@@ -23,6 +29,11 @@ public class MyrialSwordEntity extends ThrowableItemProjectile {
     public MyrialSwordEntity(Player player, Level level, ItemStack myrialSwordVacuum) {
         super(EntityRegistry.MYRIAL_SWORD_ENTITY.get(), player, level);
         this.myrialSwordVacuum = myrialSwordVacuum;
+    }
+
+    @Override
+    public boolean canHitEntity(@NotNull Entity entity) {
+        return !entitiesHit.contains(entity.getId());
     }
 
     @Override
@@ -47,16 +58,24 @@ public class MyrialSwordEntity extends ThrowableItemProjectile {
             switchToMyrialSword((Player) getOwner());
         } else {
             entityHitResult.getEntity().hurt(DamageSource.MAGIC, 7);
+            entitiesHit.add(entityHitResult.getEntity().getId());
         }
     }
 
     @Override
     public void tick() {
-        if (getOwner() != null && getOwner().position().distanceTo(position()) > 15)
+        if (getOwner() != null && getOwner().position().distanceTo(position()) > 15) {
+            entitiesHit = new IntOpenHashSet(maxEntitiesHit);
             setDeltaMovement(Vec3.ZERO);
+        }
         if (myrialSwordVacuum != null && getOwner() != null && myrialSwordVacuum.getOrCreateTag().getBoolean("minebound.return_myrial_sword"))
             setDeltaMovement(getDeltaMovement().scale(.95).add(getOwner().position().subtract(position()).normalize().scale(.5)));
         setYRot(getYRot() + 100);
+        for (int i = 0; i < maxEntitiesHit; i++) {
+            HitResult hitResult = ProjectileUtil.getHitResult(this, this::canHitEntity);
+            if (hitResult.getType() == HitResult.Type.ENTITY && getOwner() != null && !((EntityHitResult) hitResult).getEntity().is(getOwner()))
+                onHit(hitResult);
+        }
         super.tick();
     }
 
