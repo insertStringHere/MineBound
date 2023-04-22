5d8d515d3f621f1be41b7a8cd25401a59d6f2493
Matthew Kirchheimer <mattykirchh@gmail.com>
2023-04-22 18:40:46 -0400

    MBM-309 Updated parameter type

--- src/main/java/com/mineboundteam/minebound/magic/OffensiveSpells/ShieldOffensiveSpell.java
+++ src/main/java/com/mineboundteam/minebound/magic/OffensiveSpells/ShieldOffensiveSpell.java
@@ -12,7 +12,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -80,7 +79,7 @@ public class ShieldOffensiveSpell extends ActiveSpellItem {
         }
     }
 
-    protected static boolean triggerLivingAttackEvent(Player player, Entity sourceEntity, ItemStack selectedSpell, LivingAttackEvent event) {
+    protected static boolean triggerLivingAttackEvent(Player player, LivingEntity sourceEntity, ItemStack selectedSpell, LivingAttackEvent event) {
         if (selectedSpell.getItem() instanceof ShieldOffensiveSpell spell && selectedSpell.hasTag()) {
             boolean isActive = selectedSpell.getOrCreateTag().getBoolean(ACTIVE_TAG);
             if (isActive) {
