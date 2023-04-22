8d2cc2597eef180f32e0ca15fa1417bb4abf7e46
Michael Tran <solidwater969@gmail.com>
2023-04-22 16:09:34 -0400

    MBM-353 feature | install metal death laser

--- src/main/java/com/mineboundteam/minebound/item/MyrialSwordVacuum.java
+++ src/main/java/com/mineboundteam/minebound/item/MyrialSwordVacuum.java
@@ -2,6 +2,7 @@ package com.mineboundteam.minebound.item;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -13,6 +14,21 @@ public class MyrialSwordVacuum extends Item {
 
     public MyrialSwordVacuum(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void inventoryTick(@NotNull ItemStack itemStack, Level level, @NotNull Entity entity, int slotID, boolean isSelected) {
+        if (!level.isClientSide && entity instanceof Player player && (!isSelected || player.containerMenu != player.inventoryMenu)) {
+            this.itemStack.getOrCreateTag().putBoolean("minebound.return_myrial_sword", true);
+            player.getInventory().removeItem(itemStack);
+        }
+    }
+
+    @Override
+    public boolean onDroppedByPlayer(ItemStack itemStack, Player player) {
+        this.itemStack.getOrCreateTag().putBoolean("minebound.return_myrial_sword", true);
+        player.getInventory().removeItem(itemStack);
+        return false;
     }
 
     @Override
