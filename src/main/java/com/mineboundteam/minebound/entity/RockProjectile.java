786b39858034f0fa97be6bfade8d03a5d9387e3e
YonatanWN <yonatanwn@gmail.com>
2023-05-01 14:35:56 -0400

    MBM-360: Working Rock Projectile

--- src/main/java/com/mineboundteam/minebound/entity/RockProjectile.java
+++ src/main/java/com/mineboundteam/minebound/entity/RockProjectile.java
@@ -1,19 +1,20 @@
 package com.mineboundteam.minebound.entity;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
-import org.jetbrains.annotations.NotNull;
+
 
 import static com.mineboundteam.minebound.entity.registry.EntityRegistry.ROCK_PROJECTILE;
 
@@ -21,8 +22,9 @@ import static com.mineboundteam.minebound.entity.registry.EntityRegistry.ROCK_PR
 /**
  * Derived from {@link AbstractHurtingProjectile}
  * Derived from {@link FireProjectile}
+ * Derived from {@link net.minecraft.world.entity.projectile.AbstractArrow}
  */
-public class RockProjectile extends AbstractHurtingProjectile {
+public class RockProjectile extends AbstractArrow {
     private double damage;
     private double originalX;
     private double originalY;
@@ -31,10 +33,13 @@ public class RockProjectile extends AbstractHurtingProjectile {
 
     public RockProjectile(EntityType<? extends RockProjectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        this.setSoundEvent(SoundEvents.STONE_BREAK);
+        //Overwrite damage that would have been applied in AbstractArrow
+        this.setBaseDamage(0.0D);
     }
 
-    public RockProjectile(Level pLevel, LivingEntity shooter, double pX, double pY, double pZ, double pOffsetX, double pOffsetY, double pOffsetZ, double damage, int maxDistance) {
-        super(ROCK_PROJECTILE.get(), pX, pY, pZ, pOffsetX, pOffsetY, pOffsetZ, pLevel);
+    public RockProjectile(Level pLevel, LivingEntity shooter, double pX, double pY, double pZ, double damage, int maxDistance) {
+        super(ROCK_PROJECTILE.get(), pX, pY, pZ, pLevel);
         this.setOwner(shooter);
 
         this.damage = damage;
@@ -42,6 +47,9 @@ public class RockProjectile extends AbstractHurtingProjectile {
         this.originalY = pY;
         this.originalZ = pZ;
         this.maxDist = maxDistance;
+        this.setSoundEvent(SoundEvents.STONE_BREAK);
+        //Overwrite damage that would have been applied in AbstractArrow
+        this.setBaseDamage(0.0D);
     }
 
     @Override
@@ -65,10 +73,16 @@ public class RockProjectile extends AbstractHurtingProjectile {
             if (!(entity instanceof Mob) || net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.level, this)) {
                 BlockPos blockpos = pResult.getBlockPos().relative(pResult.getDirection());
                 if (this.level.isEmptyBlock(blockpos)) {
-                    this.level.explode(this, blockpos.getX(), blockpos.getY(), blockpos.getZ(), 1.0F, false, Explosion.BlockInteraction.BREAK);
+                    this.level.explode(this, blockpos.getX(), blockpos.getY(), blockpos.getZ(), 0.5F, false, Explosion.BlockInteraction.BREAK);
                 }
             }
+            this.discard();
         }
+    }
+
+    @Override
+    protected ItemStack getPickupItem() {
+        return null;
     }
 
     @Override
@@ -83,14 +97,11 @@ public class RockProjectile extends AbstractHurtingProjectile {
             }
         }
     }
-
     @Override
-    protected @NotNull ParticleOptions getTrailParticle() {
-        return (ParticleOptions) ParticleTypes.FALLING_DUST;
+    protected Entity.MovementEmission getMovementEmission() {
+        return MovementEmission.EVENTS;
     }
 
-    public boolean hurt(DamageSource pSource, float pAmount) {
-        return false;
-    }
+
 
 }
