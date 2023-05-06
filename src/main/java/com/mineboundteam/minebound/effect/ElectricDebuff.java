a851882997b11bc363151e1196f661830f46b131
Matthew Kirchheimer <mattykirchh@gmail.com>
2023-05-06 18:34:00 -0400

    MBM-316 Implemented debuff and damage source

---
+++ src/main/java/com/mineboundteam/minebound/effect/ElectricDebuff.java
@@ -0,0 +1,44 @@
+package com.mineboundteam.minebound.effect;
+
+import com.mineboundteam.minebound.MineBound;
+import com.mineboundteam.minebound.effect.registry.EffectRegistry;
+import com.mineboundteam.minebound.util.MBDamageSources;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MineBound.MOD_ID)
+public class ElectricDebuff extends MobEffect {
+    public ElectricDebuff(MobEffectCategory pCategory, int pColor) {
+        super(pCategory, pColor);
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
+        return true;
+    }
+
+    @Override
+    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+        pLivingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 2, ((pAmplifier + 1) * 2) - 1));
+    }
+
+    public static double getDmgMult(int amplifier) {
+        return 1d + ((amplifier + 1) * 0.25d);
+    }
+
+    @SubscribeEvent
+    public static void triggerEffect(LivingHurtEvent event) {
+        if (!event.getEntity().level.isClientSide() && event.getSource().getMsgId().equals(MBDamageSources.ELECTRICITY)
+                && event.getEntityLiving().hasEffect(EffectRegistry.ELECTRIC_DEBUFF.get())) {
+            MobEffectInstance effect = event.getEntityLiving().getEffect(EffectRegistry.ELECTRIC_DEBUFF.get());
+            double amount = event.getAmount() * getDmgMult(effect.getAmplifier());
+            event.setAmount((float) amount);
+        }
+    }
+}
---
+++ src/main/java/com/mineboundteam/minebound/effect/registry/EffectRegistry.java
@@ -0,0 +1,19 @@
+package com.mineboundteam.minebound.effect.registry;
+
+import com.mineboundteam.minebound.MineBound;
+import com.mineboundteam.minebound.effect.ElectricDebuff;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.awt.*;
+
+public class EffectRegistry {
+    public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS,
+            MineBound.MOD_ID);
+
+    public static final RegistryObject<MobEffect> ELECTRIC_DEBUFF = MOB_EFFECTS.register("electric_debuff",
+            () -> new ElectricDebuff(MobEffectCategory.HARMFUL, new Color(255, 119, 40).getRGB()));
+}
