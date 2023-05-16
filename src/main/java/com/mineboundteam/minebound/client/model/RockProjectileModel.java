9dbb40f49701d71b3c97e2a8fc08ba6cd984c861
Matthew Kirchheimer <mattykirchh@gmail.com>
2023-05-16 18:28:05 -0400

    MBM-161 Implemented RockProjectileModel

--- src/main/java/com/mineboundteam/minebound/client/model/RockProjectileModel.java
+++ src/main/java/com/mineboundteam/minebound/client/model/RockProjectileModel.java
@@ -1,46 +1,37 @@
 package com.mineboundteam.minebound.client.model;
 
+import com.mineboundteam.minebound.entity.RockProjectile;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 
-public class RockProjectileModel<T extends Entity> extends EntityModel<T> {
-	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "RockProjectile"), "main");
-	private final ModelPart bb_main;
+public class RockProjectileModel extends EntityModel<RockProjectile> {
+    private final ModelPart bb_main;
 
-	public RockProjectileModel() {
-		this.bb_main = createBodyLayer().bakeRoot().getChild("VoxelShapes");
-	}
+    public RockProjectileModel(ModelPart root) {
+        this.bb_main = root.getChild("bb_main");
+    }
 
-	public RockProjectileModel(ModelPart root) {
-		this.bb_main = root.getChild("bb_main");
-	}
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
 
-	public static LayerDefinition createBodyLayer() {
-		MeshDefinition meshdefinition = new MeshDefinition();
-		PartDefinition partdefinition = meshdefinition.getRoot();
+        PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -10.0F, -4.0F, 6.0F, 6.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 14).addBox(-4.0F, -10.0F, -3.0F, 8.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
+                .texOffs(22, 20).addBox(-3.0F, -11.0F, -3.0F, 6.0F, 8.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -10.0F, -4.0F, 6.0F, 6.0F, 8.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 14).addBox(-4.0F, -10.0F, -3.0F, 8.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
-		.texOffs(22, 20).addBox(-3.0F, -11.0F, -3.0F, 6.0F, 8.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        return LayerDefinition.create(meshdefinition, 64, 64);
+    }
 
-		return LayerDefinition.create(meshdefinition, 64, 64);
-	}
+    @Override
+    public void setupAnim(RockProjectile entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+    }
 
-	@Override
-	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
-	}
-
-	@Override
-	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-	}
+    @Override
+    public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+    }
 }
