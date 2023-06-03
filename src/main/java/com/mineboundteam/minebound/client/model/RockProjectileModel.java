7d8e278265d4868857f4fc4005d7ecd2737d921e
BuildTools <unconfigured@null.spigotmc.org>
2023-06-03 15:17:14 -0400

    MBM-161 Rock Projectile - Model Lowered rock model height

--- src/main/java/com/mineboundteam/minebound/client/model/RockProjectileModel.java
+++ src/main/java/com/mineboundteam/minebound/client/model/RockProjectileModel.java
@@ -19,9 +19,9 @@ public class RockProjectileModel extends EntityModel<RockProjectile> {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -10.0F, -4.0F, 6.0F, 6.0F, 8.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 14).addBox(-4.0F, -10.0F, -3.0F, 8.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
-                .texOffs(22, 20).addBox(-3.0F, -11.0F, -3.0F, 6.0F, 8.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -30.0F, -4.0F, 6.0F, 6.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 14).addBox(-4.0F, -30.0F, -3.0F, 8.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
+                .texOffs(22, 20).addBox(-3.0F, -31.0F, -3.0F, 6.0F, 8.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
